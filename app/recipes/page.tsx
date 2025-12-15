a1ca596b8a84c83a7cb2166ad54b9817e0bf8055
David Langdon <davidlangdon@Davids-MacBook-Pro.local>
2025-12-15 08:11:45 +0000

    URL encode chocolate puddings image filename to handle spaces and question mark

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -87,7 +87,7 @@ export default function RecipesPage() {
       prepTime: '30 minutes',
       cookTime: '8-13 minutes (melting time)',
       yield: '30 mini puddings',
-      image: '/images/recipes/Use this one?.jpg',
+      image: '/images/recipes/Use%20this%20one%3F.jpg',
     ingredients: [
         '100g butter, diced',
         '2 tbsp golden syrup',
